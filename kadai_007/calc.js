372e2d2e0dab0a6f14a49bfec297e93d55d7f7c1
ma-lion-327 <m.20327.m@gmail.com>
2026-04-14 22:41:05 +0900

    JavaScript課題007再提出分をアップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,14 +1,11 @@
-let num = Math.floor(Math.random() * 29);
-console.log('変数numの値は' + num + 'です');
+let num = 15;
 
-if (num === 0) {
-  console.log('変数numの値は' + num + 'なので3と5のどちらの倍数でもありません。');
-} else if (num % 3 == 0 && num % 5 == 0) {
-  console.log('変数numの値は3と5の倍数です');
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
 } else if (num % 3 == 0) {
-  console.log('変数numは3の倍数です');
+  console.log('3の倍数です');
 } else if (num % 5 == 0) {
-  console.log('変数numの値は5の倍数です');
+  console.log('5の倍数です');
 } else  {
-  console.log('変数numの値は' + num + 'なので3と5のどちらの倍数でもありません。');
+  console.log(num);
 }
